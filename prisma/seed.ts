0a6ff886ac0b6c02c6b4c30a71de3795ff84f7e2
anapairo <ana.pairo2@gmail.com>
2022-12-19 10:45:06 -0300

    feat: hotels and rooms added to seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -47,18 +47,186 @@ async function main() {
   }
 
 
-  let ticket = await prisma.ticket.findFirst();
-  if (!ticket) {
-    ticket = await prisma.ticket.create({
+  let hotels = await prisma.hotel.findMany({ include: { Rooms: true } });
+  if (!hotels) {
+    const resort = await prisma.hotel.create({
       data: {
-        enrollmentId: 2,
-        ticketTypeId: 2,
-        status: "RESERVED",
+        name: "Driven Resort",
+        image: "https://www.melhoresdestinos.com.br/wp-content/uploads/2021/04/resort-salinas-maragogi-capa-05.jpg",
+        Rooms: {
+          createMany: {
+            data: [
+              {
+                name: "101",
+                capacity: 3,
+              },
+              {
+                name: "102",
+                capacity: 3,
+              },
+              {
+                name: "103",
+                capacity: 2,
+              },
+              {
+                name: "104",
+                capacity: 3,
+              },
+              {
+                name: "105",
+                capacity: 1,
+              },
+              {
+                name: "106",
+                capacity: 2,
+              },
+              {
+                name: "107",
+                capacity: 3,
+              },
+              {
+                name: "108",
+                capacity: 1,
+              },
+              {
+                name: "109",
+                capacity: 2,
+              },
+              {
+                name: "110",
+                capacity: 3,
+              },
+            ]
+          }
+        }
       },
+      include: { Rooms: true }
     });
+
+    const palace = await prisma.hotel.create({
+      data: {
+        name: "Driven Palace",
+        image: "https://carltonhoteis.com.br/wp-content/uploads/2019/08/palace-banner.jpg",
+        Rooms: {
+          createMany: {
+            data: [
+              {
+                name: "101",
+                capacity: 1,
+              },
+              {
+                name: "102",
+                capacity: 1,
+              },
+              {
+                name: "103",
+                capacity: 1,
+              },
+              {
+                name: "104",
+                capacity: 2,
+              },
+              {
+                name: "201",
+                capacity: 1,
+              },
+              {
+                name: "202",
+                capacity: 2,
+              },
+              {
+                name: "203",
+                capacity: 2,
+              },
+              {
+                name: "204",
+                capacity: 1,
+              },
+              {
+                name: "301",
+                capacity: 2,
+              },
+              {
+                name: "302",
+                capacity: 2,
+              },
+              {
+                name: "303",
+                capacity: 2,
+              },
+              {
+                name: "304",
+                capacity: 2,
+              },
+            ]
+          }
+        }
+      },
+      include: { Rooms: true }
+    });
+
+    const world = await prisma.hotel.create({
+      data: {
+        name: "Driven World",
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg",
+        Rooms: {
+          createMany: {
+            data: [
+              {
+                name: "101",
+                capacity: 1,
+              },
+              {
+                name: "102",
+                capacity: 1,
+              },
+              {
+                name: "103",
+                capacity: 1,
+              },
+              {
+                name: "104",
+                capacity: 1,
+              },
+              {
+                name: "105",
+                capacity: 1,
+              },
+              {
+                name: "201",
+                capacity: 2,
+              },
+              {
+                name: "202",
+                capacity: 2,
+              },
+              {
+                name: "203",
+                capacity: 2,
+              },
+              {
+                name: "204",
+                capacity: 2,
+              },
+              {
+                name: "205",
+                capacity: 2,
+              },
+            ]
+          }
+        }
+      },
+      include: { Rooms: true }
+    });
+
+    hotels = [
+      resort,
+      palace,
+      world
+    ]
   }
 
-  console.log({ ticket })
+  console.log(hotels)
 }
 
 main()
